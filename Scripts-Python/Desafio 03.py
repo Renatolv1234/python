2a8b2aecf14b444d4ddb016c74d45f0a92e273b0
Renatolv1234 <renatolorenzovicente@gmail.com>
2025-05-06 19:53:26 -0300

    Estudando...

---
+++ Scripts-Python/Desafio 03.py
@@ -0,0 +1,3 @@
+num1 = input('Digite um número:')
+num2 = input('Digite mais um número:')
+print('A soma de', num1, '+', num2, 'é', int(num1) + int(num2))
---
+++ Scripts-Python/Teste 01.py
@@ -0,0 +1,4 @@
+nome = input('Qual a seu nome?')
+idade = input('Qual sua idade?')
+peso = input('Quanto você pesa?')
+print('nome:',nome,'idade:',idade,'peso:',peso)
